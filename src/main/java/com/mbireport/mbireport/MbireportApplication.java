b2d7ad036cc1bc2cf6fd1ed70e09961c49415b3c
kinkinkei <kinkinkei@gmail.com>
2022-11-07 23:37:06 +0800

    Initial commit

---
+++ src/main/java/com/mbireport/mbireport/MbireportApplication.java
@@ -0,0 +1,13 @@
+package com.mbireport.mbireport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbireportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbireportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mbireport/mbireport/MbireportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbireport.mbireport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbireportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
